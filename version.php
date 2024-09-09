acc185bdf445d14b7d54dbff34d48041ef1b6b3e
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2024-09-09 15:50:06 +0200

    Updated plugin maturity to MATURITY_STABLE.

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'qbank_filtername';
 $plugin->version   = 2024090900;
 $plugin->requires  = 2023100900;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0';
