863ceca9046ec0df198d610573611d7786cd3f60
Mateusz Walczak <mateusz.walczak@p.lodz.pl>
2024-11-14 09:23:58 +0100

    Updated plugin version and realese to 1.0.3

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbank_filtername';
-$plugin->version   = 2024091200;
+$plugin->version   = 2024111400;
 $plugin->requires  = 2023100900;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.2';
+$plugin->release = '1.0.3';
